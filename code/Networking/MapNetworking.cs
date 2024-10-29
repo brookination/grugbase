1a538728f1720c8a852c74e977286290d4ab7e69
Brooke Stevens <goblin@doinyour.mom>
2024-10-29 16:32:06 -0500

    prop networking

--- code/Networking/MapNetworking.cs
+++ code/Networking/MapNetworking.cs
@@ -17,3 +17,50 @@ public sealed class MapNetworking : Component
 		
 	}
 }
+
+public sealed class PropNetworking : Component
+{
+	/// <summary>
+	/// How big the search radius for the player should be
+	/// </summary>
+	[Property, DefaultValue(100f)] public float SphereRadius = 100f;
+	
+	[Property, DefaultValue(20f)] public float RayLength = 20f;
+	
+	/// <summary>
+	/// How fast the ray should spin
+	/// </summary>
+	[Property] public Angles SpinVelocity = new Angles( 0, 20f, 0 );
+
+	private Angles CurrentAngles;
+
+	protected override void OnFixedUpdate()
+	{
+
+		CurrentAngles += SpinVelocity * Time.Delta;
+		
+
+		var tr = Scene.Trace.Sphere( SphereRadius, WorldPosition, WorldPosition + CurrentAngles.Forward * RayLength )
+			.Run();
+
+		if ( tr.Hit )
+		{
+			var player = tr.GameObject.GetComponent<Player>();
+			if ( player == null ) return;
+
+			GameObject.Network.AssignOwnership( player.GameObject.Network.Owner );
+		}
+	}
+
+	protected override void DrawGizmos()
+	{
+		if (!Gizmo.IsSelected) return;
+		
+		CurrentAngles += SpinVelocity * Time.Delta;
+		
+		Gizmo.Draw.WorldText( "This isn't entirely accurate.\n It'll be done every fixed update, not frame, as demo'd here", global::Transform.Zero.WithPosition( Vector3.Zero + Vector3.Up * 50 ).WithRotation( Angles.Zero.WithRoll( 90f ) ).WithScale( Vector3.One*0.5f ), size:12 );
+		
+		Gizmo.Draw.Line( new Line( Vector3.Zero, CurrentAngles.Forward * RayLength  ) );
+		Gizmo.Draw.LineSphere( Vector3.Zero + CurrentAngles.Forward * RayLength, SphereRadius );
+	}
+}
