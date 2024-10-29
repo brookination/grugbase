25cbd31c8696ed81f45bd315744b0ca8e4de29dd
Brooke Stevens <goblin@doinyour.mom>
2024-10-29 13:26:26 -0500

    init

---
+++ Libraries/playercontroller/Code/PlayerUse.cs
@@ -0,0 +1,181 @@
+
+
+public sealed class PlayerUse : Component
+{
+	[RequireComponent] public Player Player { get; set; }
+
+	IPressable pressed;
+	Rigidbody carrying;
+	Transform carryTransform;
+	Transform carryOriginalTransform;
+
+	public bool Interative;
+	public string TooltipIcon;
+	public string Tooltip;
+
+	protected override void OnUpdate()
+	{
+		if ( !Player.Network.IsOwner )
+			return;
+
+		var lookingAt = TryGetLookedAt( 0.0f );
+		lookingAt ??= TryGetLookedAt( 2.0f );
+		lookingAt ??= TryGetLookedAt( 4.0f );
+		lookingAt ??= TryGetLookedAt( 8.0f );
+
+
+		if ( Input.Pressed( "use" ) )
+		{
+			if ( lookingAt is IPressable button )
+			{
+				button.Press( new IPressable.Event( this ) );
+				pressed = button;
+			}
+
+			if ( lookingAt is Rigidbody rb )
+			{
+				StartCarry( rb );
+			}
+		}
+
+		if ( Input.Released( "use" ) )
+		{
+			if ( pressed is not null )
+			{
+				pressed.Release( new IPressable.Event( this ) );
+				pressed = default;
+			}
+
+			if ( carrying is not null )
+			{
+				StopCarrying();
+			}
+		}
+
+		if ( pressed is not null || carrying.IsValid() )
+		{
+			Interative = false;
+			return;
+		}
+
+		if ( lookingAt is IPressable btn )
+		{
+			var c = (Component)btn;
+			var tt = c.GetComponent<Tooltip>();
+			if ( tt is not null )
+			{
+				Tooltip = tt.Text;
+				TooltipIcon = tt.Icon;
+			}
+			else
+			{
+				Tooltip = $"Use";
+				TooltipIcon = "pan_tool_alt";
+			}
+
+			Interative = true;
+		}
+		else if ( lookingAt is Rigidbody rbb && CanCarry( rbb ) )
+		{
+			Tooltip = "Pick Up";
+			TooltipIcon = "back_hand";
+			Interative = true;
+		}
+		else
+		{
+			Tooltip = null;
+			Interative = false;
+		}
+	}
+
+	private void StartCarry( Rigidbody rb )
+	{
+		if ( !rb.Network.TakeOwnership() )
+			return;
+
+		carrying = rb;
+		carryOriginalTransform = rb.Transform.World;
+		carryTransform = Player.EyeTransform.ToLocal( rb.Transform.World );
+	}
+
+	void StopCarrying()
+	{
+		if ( carrying.IsValid() )
+		{
+			carrying.Network.DropOwnership();
+		}
+
+		carrying = default;
+	}
+
+	protected override void OnFixedUpdate()
+	{
+		base.OnFixedUpdate();
+
+		if ( carrying.IsValid() && !carrying.IsProxy )
+		{
+			var targetTransform = Player.EyeTransform.ToWorld( carryTransform );
+			targetTransform.Rotation = carryOriginalTransform.Rotation.Angles().WithYaw( targetTransform.Rotation.Angles().yaw );
+
+			var distance = Vector3.DistanceBetween( targetTransform.Position, carrying.WorldPosition );
+
+			if ( distance > 50.0f )
+			{
+				StopCarrying();
+				return;
+			}
+
+			var mass = carrying.PhysicsBody.Mass;
+			var moveSpeed = mass.Remap( 50, 3000, 0.05f, 2.0f, true );
+
+			carrying.PhysicsBody.SmoothMove( targetTransform, moveSpeed, Time.Delta );
+		}
+	}
+
+	private bool CanCarry( Rigidbody rb )
+	{
+		if ( !rb.IsValid() ) return false;
+		if ( !rb.Network.Active ) return false;
+		if ( rb.Network.OwnerTransfer != OwnerTransfer.Takeover ) return false;
+
+		return true;
+	}
+
+	protected override void OnDisabled()
+	{
+		if ( pressed is not null )
+		{
+			pressed.Release( new IPressable.Event( this ) );
+		}
+
+		base.OnDisabled();
+	}
+
+	object TryGetLookedAt( float radius )
+	{
+		var eyeTrace = Scene.Trace
+						.Ray( Scene.Camera.Transform.World.ForwardRay, 150 )
+						.IgnoreGameObjectHierarchy( GameObject )
+						.Radius( radius )
+						.Run();
+
+		if ( !eyeTrace.Hit ) return default;
+		if ( !eyeTrace.GameObject.IsValid() ) return default;
+
+		var button = eyeTrace.GameObject.Components.Get<IPressable>();
+		if ( button is not null && button.CanPress( new IPressable.Event( this ) ) ) return button;
+
+		var rigidbody = eyeTrace.GameObject.Components.Get<Rigidbody>();
+		if ( CanCarry( rigidbody ) ) return rigidbody;
+
+		return default;
+	}
+
+}
+
+
+public class Tooltip : Component
+{
+	[Property] public string Text { get; set; }
+	[Property] public string Icon { get; set; }
+}
---
+++ code/PlayerCameraEffects.cs
@@ -0,0 +1,113 @@
+﻿using Sandbox.Diagnostics;
+
+public class PlayerCameraEffects : Component, IPlayerEvent
+{
+	List<BaseCameraShake> effects = new();
+
+	protected override void OnUpdate()
+	{
+		foreach ( var effect in effects )
+		{
+			effect.Update();
+		}
+
+		effects.RemoveAll( x => x.IsDone );
+	}
+
+	void IPlayerEvent.OnJump( Player player )
+	{
+		if ( player.IsProxy ) return;
+
+		var punch = new CameraPunch( new Vector3( -20, 0, 0 ), 0.5f, 2.0f, 1.0f );
+		effects.Add( punch );
+	}
+
+	void IPlayerEvent.OnLand( Player player, float distance, Vector3 velocity )
+	{
+		if ( player.IsProxy ) return;
+
+		var punch = new CameraPunch( new Vector3( 0.3f * distance, Random.Shared.Float( -10, 10 ), Random.Shared.Float( -10, 10 ) ), 1.0f, 1.5f, 0.7f );
+		effects.Add( punch );
+	}
+
+	void IPlayerEvent.OnCameraPostSetup( Player player, Sandbox.CameraComponent camera )
+	{
+		if ( player.IsProxy ) return;
+
+		foreach ( var effect in effects )
+		{
+			effect.ModifyCamera( camera );
+		}
+
+		MovementEffects( player, camera );
+	}
+
+	float roll;
+
+	private void MovementEffects( Player player, CameraComponent camera )
+	{
+		Assert.NotNull( player );
+		Assert.NotNull( player.Controller );
+
+		//if ( pc.BodyController.IsOnGround )
+		//{
+		//	distance += pc.BodyController.Velocity.Length * Time.Delta;
+		//}
+
+		var scaler = player.Controller.WishVelocity.Length.Remap( 0, player.Controller.RunSpeed, 0, 1 );
+
+		// bob
+		// undone: made me feel sick
+		//camera.Transform.Position += Vector3.Up * scaler * MathF.Sin( distance * 0.06f ) * 0.2f;
+
+		// side movement
+		var r = player.Controller.WishVelocity.Dot( player.EyeTransform.Left ) / -100.0f;
+		roll = MathX.Lerp( roll, r, Time.Delta * 8.0f, true );
+
+		camera.WorldRotation *= new Angles( 0, 0, roll );
+	}
+
+	abstract class BaseCameraShake
+	{
+		public virtual void Update() { }
+		public virtual bool IsDone => true;
+		public abstract void ModifyCamera( CameraComponent cc );
+	}
+
+	class CameraPunch : BaseCameraShake
+	{
+		float deathTime;
+		float lifeTime = 0.0f;
+
+		public CameraPunch( Vector3 target, float time, float frequency, float damp )
+		{
+			damping.Current = target;
+			damping.Target = 0;
+			damping.SmoothTime = time;
+			damping.Frequency = frequency;
+			damping.Damping = damp;
+
+			deathTime = damping.SmoothTime;
+		}
+
+		public override bool IsDone => deathTime <= 0;
+
+		SpringDampVector3 damping;
+
+		public override void Update()
+		{
+			deathTime -= Time.Delta;
+			lifeTime += Time.Delta;
+
+			damping.Update( Time.Delta );
+		}
+
+		public override void ModifyCamera( CameraComponent cc )
+		{
+			var amount = lifeTime.Remap( 0, 0.3f, 0, 1 );
+
+			cc.WorldRotation *= new Angles( damping.Current * amount );
+		}
+	}
+
+}
