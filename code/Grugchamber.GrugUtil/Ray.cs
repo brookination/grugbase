786d7dcfc088669530ae1a7016b7d8ce405749d6
Brooke Stevens <goblin@doinyour.mom>
2024-10-30 14:47:57 -0500

    add weaponfx to grugutil

--- code/Grugchamber.GrugUtil/Ray.cs
+++ code/Grugchamber.GrugUtil/Ray.cs
@@ -2,6 +2,10 @@
 {
 	public static class Rays
 	{
+		/// <summary>
+		/// Trace a ray from the scene's main camera's center.
+		/// </summary>
+		/// <returns></returns>
 		public static SceneTraceResult TraceFromCenter()
 		{
 			var ray = Game.ActiveScene.Trace.Ray(
@@ -10,6 +14,61 @@
 			return ray.Run();
 
 		}
+	}
+
+	public static class WeaponEffects
+	{
+		/// <summary>
+		/// Create a bullet hole
+		/// </summary>
+		/// <param name="HitPosition"></param>
+		/// <param name="HitNormal"></param>
+		/// <param name="surface"></param>
+		public static async void BulletHoleFx( Vector3 HitPosition, Vector3 HitNormal, Surface surface, GameObject hitObject )
+		{
+			Scene scene = Game.ActiveScene;
+
+			GameObject holeObj = new GameObject();
+
+			string BulletHolePath = String.Empty;
+			
+			
+			foreach ( var keys in DecalEffectsKey )
+			{
+				if ( keys.Key == "everythingelse" )
+				{
+					BulletHolePath = keys.Value;
+				}
+
+				if ( keys.Key == surface.ResourcePath )
+				{
+					BulletHolePath = keys.Value;
+				}
+			}
+			
+			// safety first
+			if (BulletHolePath == string.Empty) return;
+			
+			holeObj.SetParent( hitObject );
+
+			holeObj.WorldTransform = new Transform( HitPosition + HitNormal * 2.0f, Rotation.LookAt( HitNormal ) );
+
+			var decalRenderer = holeObj.AddComponent<DecalRenderer>();
+
+			decalRenderer.Material = await Material.LoadAsync( BulletHolePath );
+			
+			return;
+
+
+
+		}
+
+		public static Dictionary<string, string> DecalEffectsKey = new Dictionary<string, string>
+		{
+			{"everythingelse", "materials/decals/bullethole.vmat" },
+			{"surfaces/flesh.surface", "materials/decals/flesh/flesh1.vmat"}
+		};
+		
 	}
 }
 
