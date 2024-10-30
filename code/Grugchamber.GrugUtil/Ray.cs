550a4f09538253d3ca252d604fc66423859ad3dd
Brooke Stevens <goblin@doinyour.mom>
2024-10-30 14:53:21 -0500

    would be cleaner if i did it in the weapon itself

--- code/Grugchamber.GrugUtil/Ray.cs
+++ code/Grugchamber.GrugUtil/Ray.cs
@@ -18,56 +18,17 @@
 
 	public static class WeaponEffects
 	{
-		/// <summary>
-		/// Create a bullet hole
-		/// </summary>
-		/// <param name="HitPosition"></param>
-		/// <param name="HitNormal"></param>
-		/// <param name="surface"></param>
-		public static async void BulletHoleFx( Vector3 HitPosition, Vector3 HitNormal, Surface surface, GameObject hitObject )
-		{
-			Scene scene = Game.ActiveScene;
-
-			GameObject holeObj = new GameObject();
-
-			string BulletHolePath = String.Empty;
-			
-			
-			foreach ( var keys in DecalEffectsKey )
-			{
-				if ( keys.Key == "everythingelse" )
-				{
-					BulletHolePath = keys.Value;
-				}
-
-				if ( keys.Key == surface.ResourcePath )
-				{
-					BulletHolePath = keys.Value;
-				}
-			}
-			
-			// safety first
-			if (BulletHolePath == string.Empty) return;
-			
-			holeObj.SetParent( hitObject );
-
-			holeObj.WorldTransform = new Transform( HitPosition + HitNormal * 2.0f, Rotation.LookAt( HitNormal ) );
-
-			var decalRenderer = holeObj.AddComponent<DecalRenderer>();
-
-			decalRenderer.Material = await Material.LoadAsync( BulletHolePath );
-			
-			return;
-
-
-
-		}
 
 		public static Dictionary<string, string> DecalEffectsKey = new Dictionary<string, string>
 		{
 			{"everythingelse", "materials/decals/bullethole.vmat" },
 			{"surfaces/flesh.surface", "materials/decals/flesh/flesh1.vmat"}
 		};
+
+		public static string GetDecalFromSurface( string surfacePath )
+		{
+			return DecalEffectsKey[surfacePath];
+		} 
 		
 	}
 }
