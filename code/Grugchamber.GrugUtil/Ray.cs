549241d76f99bf3168b6726cffb33e9e51fc23c8
Brooke Stevens <goblin@doinyour.mom>
2024-10-30 15:14:37 -0500

    cleanup grugutil

--- code/Grugchamber.GrugUtil/Ray.cs
+++ code/Grugchamber.GrugUtil/Ray.cs
@@ -1,20 +1,16 @@
-﻿namespace Grugchamber.GrugUtil
+﻿namespace Grugchamber.GrugUtil;
+
+public static class Rays
 {
-	public static class Rays
+	/// <summary>
+	///     Trace a ray from the active scene's main camera's center.
+	/// </summary>
+	/// <returns></returns>
+	public static SceneTraceResult TraceFromCenter()
 	{
-		/// <summary>
-		/// Trace a ray from the scene's main camera's center.
-		/// </summary>
-		/// <returns></returns>
-		public static SceneTraceResult TraceFromCenter()
-		{
-			var ray = Game.ActiveScene.Trace.Ray(
-				Game.ActiveScene.Camera.ScreenPixelToRay( new Vector2( Screen.Width / 2, Screen.Height / 2 ) ), 1000f );
+		var ray = Game.ActiveScene.Trace.Ray(
+			Game.ActiveScene.Camera.ScreenPixelToRay( new Vector2( Screen.Width / 2, Screen.Height / 2 ) ), 1000f );
 
-			return ray.Run();
-
-		}
+		return ray.Run();
 	}
-
 }
-
--- code/Grugchamber.GrugUtil/WeaponEffects.cs
+++ code/Grugchamber.GrugUtil/WeaponEffects.cs
@@ -3,16 +3,16 @@
 public static class WeaponEffects
 {
 	/// <summary>
-	/// The dictionary of decal effects, the key is the surface resource path, the value is the decal path.
+	///     The dictionary of decal effects, the key is the surface resource path, the value is the decal path.
 	/// </summary>
-	public static readonly Dictionary<string, string> DecalEffectsKey = new Dictionary<string, string>()
+	public static readonly Dictionary<string, string> DecalEffectsKey = new()
 	{
 		{ "everything", "materials/decals/bullethole.vmat" },
 		{ "surfaces/flesh.surface", "materials/decals/flesh/flesh1.vmat" }
 	};
 
 	/// <summary>
-	/// Get the bullet hole decal from surface.
+	///     Get the bullet hole decal from surface.
 	/// </summary>
 	/// <param name="surfacePath"></param>
 	/// <returns></returns>
