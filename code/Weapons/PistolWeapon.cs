e7dcdca69e6bde19c54f29976af6c3f31ab1ae27
Brooke Stevens <goblin@doinyour.mom>
2024-10-29 21:18:59 -0500

    add PistolWeapon to learn how walker weapons work

---
+++ code/Weapons/PistolWeapon.cs
@@ -0,0 +1,17 @@
+﻿using Grugchamber.GrugUtil;
+
+public sealed class PistolWeapon : BaseWeapon, IPlayerEvent
+{
+	public override void OnControl( Player player )
+	{
+		if ( Input.Pressed( "attack1" ) )
+		{
+			var trace = Rays.TraceFromCenter();
+
+			if ( trace.Hit )
+			{
+				Log.Info( "Woah" );
+			}
+		}
+	}
+}
