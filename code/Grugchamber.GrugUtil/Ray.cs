0b322e78522275324c42e6f97cee5da5330a3326
Brooke Stevens <goblin@doinyour.mom>
2024-10-29 21:19:50 -0500

    grugutil

---
+++ code/Grugchamber.GrugUtil/Ray.cs
@@ -0,0 +1,15 @@
+﻿namespace Grugchamber.GrugUtil
+{
+	public static class Rays
+	{
+		public static SceneTraceResult TraceFromCenter()
+		{
+			var ray = Game.ActiveScene.Trace.Ray(
+				Game.ActiveScene.Camera.ScreenPixelToRay( new Vector2( Screen.Height / 2, Screen.Width / 2 ) ), 1000f );
+
+			return ray.Run();
+
+		}
+	}
+}
+
