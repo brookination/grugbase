f47f7123548eca3a3bdd45fc0341c6a3bad24a56
Brooke Stevens <goblin@doinyour.mom>
2024-11-02 18:06:32 -0500

    viewmodel's done

--- code/Viewmodel/ViewmodelManager.cs
+++ code/Viewmodel/ViewmodelManager.cs
@@ -14,21 +14,20 @@ public sealed class ViewmodelManager : Component
 	void CheckViewmodel()
 	{
 		
-		// check if player is alive; if not null
 		if (Player == null || Player.IsDead)
-		{
-			ViewmodelEnabled = false;
-		}
-		else
 		{
 			ViewmodelEnabled = true;
 		}
+		else
+		{
+			ViewmodelEnabled = false;
+		}
 
-		// is viewmodel there?
-		if (ViewmodelEnabled)
+		if (!ViewmodelEnabled)
 		{
 			ViewmodelEnabled = PlayerInventory.ActiveWeapon?.WeaponViewmodel == null;
 		}
+
 		
 		
 		
@@ -43,13 +42,77 @@ public sealed class ViewmodelManager : Component
 	
 	void OnViewmodelChanged()
 	{
-		Log.Info( _viewmodelEnabled );
+		if ( !ViewmodelEnabled )
+		{
+			Log.Info( "Creating viewmodel" );
+			CreateViewmodel();
+		}
+		else
+		{
+			Log.Info( "Destroying viewmodel" );
+			DestroyViewmodel();
+		}
 	}
-	
+
+	void CreateViewmodel()
+	{
+		if ( ViewmodelObject == null && !ViewmodelEnabled )
+		{
+			ViewmodelObject = new GameObject();
+			
+			ViewmodelObject.SetParent( Scene.Camera.GameObject );
+
+			ViewmodelObject.LocalTransform = global::Transform.Zero;
+			
+			
+			var armObj = new GameObject();
+			var weaponObj = new GameObject();
+			
+			armObj.SetParent(  ViewmodelObject );
+			armObj.LocalTransform = global::Transform.Zero;
+			
+			weaponObj.SetParent( ViewmodelObject );
+			weaponObj.LocalTransform = global::Transform.Zero;
+			
+			var armRenderer = armObj.AddComponent<SkinnedModelRenderer>(  );
+			var weaponRenderer = weaponObj.AddComponent<SkinnedModelRenderer>();
+
+			armRenderer.Model = Model.Load( "models/first_person/first_person_arms.vmdl" );
+			weaponRenderer.Model = PlayerInventory.ActiveWeapon.WeaponViewmodel;
+
+			armRenderer.RenderOptions.Game = false;
+			armRenderer.RenderOptions.Overlay = true;
+			
+			weaponRenderer.RenderOptions.Game = false;
+			weaponRenderer.RenderOptions.Overlay = true;
+
+			armRenderer.RenderType = ModelRenderer.ShadowRenderType.Off;
+			weaponRenderer.RenderType = ModelRenderer.ShadowRenderType.Off;
+
+			armRenderer.BoneMergeTarget = weaponRenderer;
+
+			var animator = weaponObj.AddComponent<ViewmodelAnimator>();
+			animator.Target = weaponRenderer;
+
+
+		}
+	}
+
+	void DestroyViewmodel()
+	{
+		if ( ViewmodelObject != null )
+		{
+			
+			ViewmodelObject.DestroyImmediate();
+			ViewmodelObject = null;
+		}
+	}
 	
 	
 	protected override void OnFixedUpdate()
 	{
+		if (IsProxy) return;
+		
 		CheckViewmodel();
 
 		
